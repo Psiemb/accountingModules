3b68e86e1bdb34e8f385c1eed0cceae084ce6d23
Piotr S <psiemb@tlen.pl>
2021-05-19 00:00:03 +0200

    nice refactor

--- src/main/java/com/cwiczenia/ksiegowanie/mapper/ExpenseResponseMapper.java
+++ src/main/java/com/cwiczenia/ksiegowanie/mapper/ExpenseResponseMapper.java
@@ -1,23 +1,26 @@
 package com.cwiczenia.ksiegowanie.mapper;
 
-import com.cwiczenia.ksiegowanie.entity.ExpenseWEWNETRZNY_MODEL;
+import com.cwiczenia.ksiegowanie.entity.ExpenseInternalEntity;
 import com.cwiczenia.ksiegowanie.response.ExpenseResponse;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Objects;
 
 @Component
 public class ExpenseResponseMapper {
 
-    public ExpenseResponse mapToResponse(List<ExpenseWEWNETRZNY_MODEL> expenseWEWNETRZNY_models){
+    public ExpenseResponse mapToResponse(List<ExpenseInternalEntity> source) {
+        if (Objects.isNull(source) || Objects.isNull(source.get(0)) || source.get(0).getCostValue() < 0) {
+            // throw exeption
+            // return new ExpenseResponse();
+            return null;
+        }
+        int costValue = source.get(0).getCostValue();
 
-        ExpenseWEWNETRZNY_MODEL expenseWEWNETRZNY_model = expenseWEWNETRZNY_models.get(0);
         ExpenseResponse resultToBeReturn = new ExpenseResponse();
-
-        resultToBeReturn.setCostValueR(expenseWEWNETRZNY_model.getCostValue());
+        resultToBeReturn.setCostValueR(costValue);
 
         return resultToBeReturn;
     }
-
-
 }
---
+++ src/test/java/com/cwiczenia/ksiegowanie/mapper/ExpenseResponseMapperTest.java
@@ -0,0 +1,82 @@
+package com.cwiczenia.ksiegowanie.mapper;
+
+import com.cwiczenia.ksiegowanie.entity.ExpenseInternalEntity;
+import com.cwiczenia.ksiegowanie.response.ExpenseResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+class ExpenseResponseMapperTest {
+
+    private ExpenseResponseMapper expenseResponseMapper;
+
+    @BeforeEach
+    void setUp() {
+        this.expenseResponseMapper = new ExpenseResponseMapper();
+    }
+
+    @Test
+    void returnNullWhenListEqualsNull() {
+        assertNull(expenseResponseMapper.mapToResponse(null));
+    }
+
+    @Test
+    void returnNullWhenListHasNullElement() {
+        // given
+        List<ExpenseInternalEntity> list = Arrays.asList(null, new ExpenseInternalEntity());
+
+        // when
+        ExpenseResponse result = expenseResponseMapper.mapToResponse(list);
+
+        // then
+        assertNull(result);
+    }
+
+    @Test
+    void returnZeroWhenDefaultValueInFirstElement() {
+        // given
+        List<ExpenseInternalEntity> list = Arrays.asList(new ExpenseInternalEntity(), new ExpenseInternalEntity());
+
+        // when
+        ExpenseResponse result = expenseResponseMapper.mapToResponse(list);
+
+        // then
+        assertEquals(0, result.getCostValueR());
+    }
+
+    @Test
+    void mapCostValueFromFirstElement() {
+        // given
+        ExpenseInternalEntity item = new ExpenseInternalEntity();
+        item.setCostValue(100);
+
+        List<ExpenseInternalEntity> list = Arrays.asList(item, new ExpenseInternalEntity());
+
+        // when
+        ExpenseResponse result = expenseResponseMapper.mapToResponse(list);
+
+        //then
+        assertEquals(100, result.getCostValueR());
+    }
+
+    @Test
+    void returnZeroWhenCostValueIsNegative() {
+        // given
+        ExpenseInternalEntity item = new ExpenseInternalEntity();
+        item.setCostValue(-100);
+
+        List<ExpenseInternalEntity> list = Arrays.asList(item, new ExpenseInternalEntity());
+
+        // when
+        ExpenseResponse result = expenseResponseMapper.mapToResponse(list);
+
+        //then
+        assertNull(result);
+    }
+
+}
